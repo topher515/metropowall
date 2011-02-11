0b8ffc10437ff0a580ee061cd7734934858dabed
Chris Wilcox <topher515@gmail.com>
2011-02-11 11:20:45 -0800

    Wall simulator functional

--- wallsim.py
+++ wallsim.py
@@ -12,10 +12,10 @@ from walldriver import PANEL_NUM
 		
 
 WALL_LAYOUT = (
-(None,0,4,8,12,16,20,24,28,None),
-(None,1,5,9,13,17,21,25,29,None),
-(32,  2,6,10,14,18,22,26,30,34),
-(33,  3,7,11,15,19,23,27,31,35)
+(None,1,5,9, 13,17,21,25,29,None),
+(None,2,6,10,14,18,22,26,30,None),
+(33,  3,7,11,15,19,23,27,31,35),
+(34,  4,8,12,16,20,24,28,32,36)
 )
 
 
@@ -26,11 +26,11 @@ class WallSimulator(object):
 		def __init__(self,panel,channel_num):
 			self._value = 0
 			self.panel = panel
-			self.channel_number = channel_num
+			self.channel_num = channel_num
 		
 		def _set_val(self,val):
 			self._value =val
-			Tk().generate_event('<<Channel-Update-%s>>' % channel_num)
+			self.panel.canvas.master.event_generate('<<Channel-Update-%s>>' % self.channel_num)
 		def _get_val(self):
 			return self._value
 		value = property(fget=_get_val,fset=_set_val)
@@ -44,23 +44,21 @@ class WallSimulator(object):
 			self.g = WallSimulator.Channel(self,panel_num*3+2)
 			self.b = WallSimulator.Channel(self,panel_num*3+3)
 			
-		def _update(self):
-			canvas.itemconfig(self.rect,fill='#%0.2X%0.2X%0.2X' % (self.r.value,self.g.value,self.b.value))
+		def _update(self,foo):
+			self.canvas.itemconfig(self.rect,fill='#%0.2X%0.2X%0.2X' % (self.r.value,self.g.value,self.b.value))
 
 
 			
 	
 	
-	def __init__(self):
+	def __init__(self,host="localhost",port=7778):
 		
 		# Setup TCP server
-		self.host = "localhost"
-		self.port = 5005
+		self.host = host
+		self.port = port
 		self.server = WallSimulator.WallTCPServer((self.host,self.port), WallSimulator.WallTCPHandler, wall=self)
 		#super(WallServer,self).__init__()
 		self.server_thread = threading.Thread(target=self.server.serve_forever)
-		
-		self.debug_thread = threading.Thread(target=mainloop)
 		
 		# Setup GUI
 		self.master = Tk()
@@ -79,20 +77,23 @@ class WallSimulator(object):
 			x = 1
 			for panel_num in row:
 				
-				i = self.w.create_rectangle(x, y, panel_w, panel_h, fill='#000')
+				i = self.w.create_rectangle(x, y, x+panel_w, y+panel_h, fill='#000')
 				
-				if panel_num:
+				if panel_num is not None:
+					
+					j = self.w.create_text(x+10,y+10,text="%s" % panel_num,fill="#FFF")
+					
 					p = WallSimulator.Panel(canvas=self.w, rect=i, panel_num=panel_num)
 				
-					self.panels[panel_num+1] = p
+					self.panels[panel_num] = p
 					
-					j = panel_num*3+1
+					j = (panel_num-1)*3+1
 					self.channels[j] = p.r
 					self.master.bind('<<Channel-Update-%s>>' % j, p._update)
-					j = panel_num*3+2
+					j = (panel_num-1)*3+2
 					self.channels[j] = p.g
 					self.master.bind('<<Channel-Update-%s>>' % j, p._update)
-					j = panel_num*3+3
+					j = (panel_num-1)*3+3
 					self.channels[j] = p.b
 					self.master.bind('<<Channel-Update-%s>>' % j, p._update)
 				
@@ -100,13 +101,20 @@ class WallSimulator(object):
 			
 			y += panel_h+2	
 
+		print "Built Wall GUI..."
+		#print "Channels..."
+		#print self.channels
+		#print "Panels..."
+		#print self.panels
+
 
 	def start(self):	
-		#print "Starting server at %s:%s..." % (self.host,self.port)
-		#self.server_thread.start()
+		print "Starting server at %s:%s..." % (self.host,self.port)
+		self.server_thread.start()
 		print "Starting GUI..."
-		#self.mainloop()
-		self.debug_thread.start()
+		mainloop()
+		print "Stopping server..."
+		self.server.shutdown()
 
 	
 	class WallTCPServer(SocketServer.TCPServer):
@@ -117,8 +125,9 @@ class WallSimulator(object):
 
 	class WallTCPHandler(SocketServer.StreamRequestHandler):
 		def handle(self):
-			channel,value = self.rfile.readline().strip().split(' ')
-			self.server.wall.channels[int(channel)] = int(value)
+			lines = self.rfile.readlines()
+			for ch,val in [line.strip().split(' ') for line in lines]:
+				self.server.wall.channels[int(ch)].value = int(val)
 	
 		
 def main():
